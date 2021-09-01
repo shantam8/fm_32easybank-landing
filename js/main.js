cae860fc79cb51da7351429dee6ba4a8c6373e6d
shantam8 <dscharmen@aol.com>
2021-09-01 14:19:10 +0200

    mobile done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
+let btnMobileMenu = document.querySelector("#iconMobileMenu");
+let mobileMenuOverlay = document.querySelector("#mobileNavBgOverlay");
+let mobileMenuBlock = document.querySelector("#mobileNavBlock");
+let isMobileMenuOpen = false;
 
+function toggleMobileMenu() {
+  if (isMobileMenuOpen) {
+    mobileMenuOverlay.classList.remove("opened");
+    mobileMenuOverlay.classList.add("closed");
+    btnMobileMenu.style.backgroundImage = "url('../images/icon-hamburger.svg')";
+    isMobileMenuOpen = !isMobileMenuOpen;
+  } else {
+    mobileMenuOverlay.classList.remove("displayNoneStartClass");
+    mobileMenuOverlay.classList.remove("closed");
+    mobileMenuOverlay.classList.add("opened");
+    btnMobileMenu.style.backgroundImage = "url('../images/icon-close.svg')";
 
-
-function init(){
-    console.log("Hi");
+    isMobileMenuOpen = !isMobileMenuOpen;
+  }
 }
 
-
+function init() {
+  btnMobileMenu.addEventListener("click", toggleMobileMenu);
+}
 
 window.onload = init();
