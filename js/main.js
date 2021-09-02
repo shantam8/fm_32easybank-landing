fa255851c2b602341afbe69aac99764422cead95
shantam8 <dscharmen@aol.com>
2021-09-02 22:46:57 +0200

    mobile done, desktop almost

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ function toggleMobileMenu() {
   if (isMobileMenuOpen) {
     mobileMenuOverlay.classList.remove("opened");
     mobileMenuOverlay.classList.add("closed");
+    document.querySelector("body").classList.remove("overflowHidden");;
     btnMobileMenu.style.backgroundImage = "url('../images/icon-hamburger.svg')";
     isMobileMenuOpen = !isMobileMenuOpen;
   } else {
     mobileMenuOverlay.classList.remove("displayNoneStartClass");
     mobileMenuOverlay.classList.remove("closed");
     mobileMenuOverlay.classList.add("opened");
+    document.querySelector("body").classList.add("overflowHidden");;
     btnMobileMenu.style.backgroundImage = "url('../images/icon-close.svg')";
 
     isMobileMenuOpen = !isMobileMenuOpen;
