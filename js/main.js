baf764268d54a62156656879bfb7cbc81c21a061
shantam8 <dscharmen@aol.com>
2021-09-06 14:26:59 +0200

    design, tab, aria, all done

--- js/main.js
+++ js/main.js
@@ -7,22 +7,30 @@ function toggleMobileMenu() {
   if (isMobileMenuOpen) {
     mobileMenuOverlay.classList.remove("opened");
     mobileMenuOverlay.classList.add("closed");
-    document.querySelector("body").classList.remove("overflowHidden");;
+    document.querySelector("body").classList.remove("overflowHidden");
     btnMobileMenu.style.backgroundImage = "url('../images/icon-hamburger.svg')";
+    btnMobileMenu.setAttribute("aria-label", "open   mobile menu");;
     isMobileMenuOpen = !isMobileMenuOpen;
   } else {
     mobileMenuOverlay.classList.remove("displayNoneStartClass");
     mobileMenuOverlay.classList.remove("closed");
     mobileMenuOverlay.classList.add("opened");
-    document.querySelector("body").classList.add("overflowHidden");;
+    document.querySelector("body").classList.add("overflowHidden");
     btnMobileMenu.style.backgroundImage = "url('../images/icon-close.svg')";
-
+    btnMobileMenu.setAttribute("aria-label", "close mobile menu");;
     isMobileMenuOpen = !isMobileMenuOpen;
+  }
+}
+
+function closeMobileMenuOnResize() {
+  if (isMobileMenuOpen && window.innerWidth > 800) {
+    toggleMobileMenu();
   }
 }
 
 function init() {
   btnMobileMenu.addEventListener("click", toggleMobileMenu);
+  window.addEventListener("resize", closeMobileMenuOnResize);
 }
 
 window.onload = init();
